1afbfc10fcd7109ddeadeadd0d9a29ceff337fd5
Tuxuy3Dom <ihor.yaremchuk2120@gmail.com>
2023-07-01 14:02:07 +0200

    Add include html page, and modal menu open

--- README.md
+++ README.md
@@ -7,14 +7,14 @@ a website will be created based on the problems of the kianit company.
 ### Frontend
 Use HTML5 [HTML5](https://www.w3schools.com/html/default.asp).
 Use CSS [CSS](https://www.w3schools.com/css/default.asp).
-Use JavaScript [JavaScript] (https://www.w3schools.com/js/default.asp).
+Use JavaScript [JavaScript](https://www.w3schools.com/js/default.asp).
 
 ### Backend
-Use Python [Python] (https://www.w3schools.com/python/default.asp).
-Use Framework Django [Django] (https://www.w3schools.com/django/index.php).
+Use Python [Python](https://www.w3schools.com/python/default.asp).
+Use Framework Django [Django](https://www.w3schools.com/django/index.php).
 
 ### Database
-Use MySQL [MySQL] (https://www.w3schools.com/mysql/default.asp).
+Use MySQL [MySQL](https://www.w3schools.com/mysql/default.asp).
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,324 @@
+const mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+});
+
+// cart
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
+const viemAll = document.querySelectorAll('.viem-all');
+const navigationLink = document.querySelectorAll('.navigation-link:not(.viem-all)');
+const longGoodList = document.querySelector('.long-goods-list');
+const showAcsessories = document.querySelectorAll('.show-acsessories');
+const showClothing = document.querySelectorAll('.show-clothing');
+const cartTableGoods = document.querySelector('.cart-table__goods');
+const cardTableTotal = document.querySelector('.card-table__total');
+const cartCount = document.querySelector('.cart-count');
+const btnDanger = document.querySelector('.btn-danger');
+
+const checkGoods = () => {
+
+    const data = [];
+
+    return async () => { //async - funkcja asynchroniczna | await - czekamy na wykonanie operacji 
+        if (data.length) return data;
+        
+        const result = await fetch('db/db.json'); //fetch - API ustawione w przeglądarce umie zaprosić dane z servera po URL | zwraca promis - obetnica przejścia odpowiedzi od servera
+        if (!result.ok) {
+            throw 'Error: ' + result.status; //throw - wyjątek
+        } 
+        data.push(...(await result.json()));
+
+        return data;
+    };
+};
+
+const getGoods = checkGoods();
+
+const cart = {
+	cartGoods: JSON.parse(localStorage.getItem('cartKey')) || [],
+	updateLocalStorage() {
+		localStorage.setItem('cartKey', JSON.stringify(this.cartGoods));
+	},
+	getCountCartGoods() {
+		return this.cartGoods.length;
+	},
+    countQuantity() {
+        const count = this.cartGoods.reduce((sum, item) => {
+            return sum + item.count;
+        }, 0);
+		cartCount.textContent = count ? count : '';
+    },
+    clearCart() {
+        this.cartGoods.length = 0;
+        this.countQuantity();
+		this.updateLocalStorage();
+        this.renderCart();
+    },
+	renderCart() {
+		cartTableGoods.textContent = '';
+		this.cartGoods.forEach(({ id, name, price, count }) => {
+			const trGood = document.createElement('tr');
+			trGood.className = 'cart-item';
+			trGood.dataset.id = id;
+
+			trGood.innerHTML = `
+				<td>${name}</td>
+				<td>$${price}</td>
+				<td><button class="cart-btn-minus">-</button></td>
+				<td>${count}</td>
+				<td><button class="cart-btn-plus">+</button></td>
+				<td>${price * count}$</td>
+				<td><button class="cart-btn-delete">x</button></td>
+			`;
+			cartTableGoods.append(trGood);
+		});
+
+		const totalPrice = this.cartGoods.reduce((sum, item) => {
+			return sum + (item.price * item.count);
+		}, 0);
+		cardTableTotal.textContent = totalPrice + '$';
+	},
+	deleteGood(id) {
+		this.cartGoods = this.cartGoods.filter(item => id !== item.id);
+		this.renderCart();
+        this.countQuantity();
+		this.updateLocalStorage();
+	},
+	minusGood(id) {
+		for (const item of this.cartGoods) {
+			if (item.id === id) {
+				if(item.count <= 1) {
+					this.deleteGood(id);
+				} else {
+					item.count--;
+				}
+				break;
+			}
+		}
+		this.renderCart();
+        this.countQuantity();
+		this.updateLocalStorage();
+	},
+	plusGood(id) {
+		for (const item of this.cartGoods) {
+			if (item.id === id) {
+				item.count++;
+				break;
+			}
+		}
+		this.renderCart();
+        cart.countQuantity();
+		this.updateLocalStorage();
+	},
+	addCartGoods(id) {
+		const goodItem = this.cartGoods.find(item => item.id ===id);
+		if (goodItem) {
+			this.plusGood(id);
+		} else {
+			getGoods()
+				.then(data => data.find(item => item.id === id))
+				.then(({ id , name, price }) => {
+					this.cartGoods.push({
+						id, 
+						name,
+						price,
+						count: 1
+					});
+                    this.countQuantity();
+					this.updateLocalStorage();
+				});
+		}
+	},
+}
+
+btnDanger.addEventListener('click', cart.clearCart.bind(cart));
+
+document.body.addEventListener('click', event => {
+	const addToCart = event.target.closest('.add-to-cart');
+
+	if(addToCart) {
+		cart.addCartGoods(addToCart.dataset.id);
+	}
+})
+
+//Destruktarycaja elementów
+cartTableGoods.addEventListener('click', (event) => {
+	const target =  event.target;
+
+	if (target.classList.contains('cart-btn-delete')) {
+		const id = target.closest('.cart-item').dataset.id;
+		cart.deleteGood(id);
+	};
+	if (target.classList.contains('cart-btn-minus')) {
+		const id = target.closest('.cart-item').dataset.id;
+		cart.minusGood(id);
+	}
+	if (target.classList.contains('cart-btn-plus')) {
+		const id = target.closest('.cart-item').dataset.id;
+		cart.plusGood(id);
+	}
+});
+
+const openModal = () => {
+	cart.renderCart();
+	modalCart.classList.add('show');
+};
+
+const closeModal = () => {
+	modalCart.classList.remove('show');
+};
+
+buttonCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
+
+modalCart.addEventListener('click', (event) => {
+	const target = event.target;
+	if (target.classList.contains('overlay') || target.classList.contains('modal-close')) { //contains - checking for the presence of a class  
+		closeModal();
+	}
+});
+
+
+
+// goods //Destruktaryzacja
+const createCard = ({ label, name, img, description, id, price }) => {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6'; 
+
+	card.innerHTML = `
+	<div class="goods-card">
+		${label ? 
+			`<span class="label">${label}</span>` : 
+			''}
+		<img src="db/${img}" alt="${name}" class="goods-image">
+		<h3 class="goods-title">${name}</h3>
+		<p class="goods-description">${description}</p>
+		<button class="button goods-card-btn add-to-cart" data-id="${id}">
+			<span class="button-price">$${price}</span>
+		</button>
+	</div>
+	`;
+
+	return card;
+}
+
+//Show card on page
+const renderCards = (data) => {
+	longGoodList.textContent = '';
+	const cards = data.map(createCard); 
+	longGoodList.append(...cards);
+	document.body.classList.add('show-goods');
+};
+
+
+const showAll = (event) => { event.preventDefault(); getGoods().then(renderCards); };
+viemAll.forEach((elem) => {
+	elem.addEventListener('click', showAll);
+});
+
+const filterCards = (field, value) => {
+	getGoods()
+		.then((data) => data.filter((good) => good[field] === value))
+		.then(renderCards);
+};
+
+navigationLink.forEach((link) => {
+	link.addEventListener('click', (event) => {
+		event.preventDefault();
+		const field = link.dataset.field;
+		const value = link.textContent;
+		filterCards(field, value);
+	});
+});
+
+showAcsessories.forEach(item => {
+	item.addEventListener('click', event => {
+		event.preventDefault();
+		filterCards('category', 'Accessories');
+	});
+});
+
+showClothing.forEach(item => {
+	item.addEventListener('click', event => {
+		event.preventDefault();
+		filterCards('category', 'Clothing');
+	});
+});
+
+// Work with OpenServer
+
+const modalForm = document.querySelector('.modal-form');
+
+const postData = dataUser => fetch('server.php', {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json'
+	},
+	body: dataUser,
+});
+
+const validForm = (formData) => {
+	let valid = false;
+
+	for (const [,value] of formData) {
+		if (value.trim()) {
+			valid = true;
+		} else {
+			valid = false;
+			break;
+		}
+	}
+
+	return valid;
+};
+
+modalForm.addEventListener('submit', event => {
+	event.preventDefault();
+	const formData = new FormData(modalForm);
+
+	if (validForm(formData) && cart.getCountCartGoods()) {
+		const data = {};
+
+		for (const [name, value] of formData) {
+			data[name] = value;
+		}
+
+		data.cart = cart.cartGoods;
+ 
+		postData(JSON.stringify(data))
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(response.status);
+				}
+				alert('Your order has been successfully sent, and you will be contacted as soon as possible.');
+				console.log(response.statusText);
+			})
+			.catch(err => {
+				alert('Unfortunately an error occurred, please try again later.');
+				console.error(err);
+			})
+			.finally(() => {
+				closeModal();
+				modalForm.reset();
+				cart.clearCart();
+			});
+	} else {
+		if (!cart.getCountCartGoods()) {
+			alert('The shopping cart is empty, add the product to the shopping cart');
+		}
+		if (!validForm(formData)) {
+			alert('Fill in the fields correctly');
+		}
+	}
+});
+
 // $getJSON('data.json', function(data) {
 //     var output = "<ul";
 //     for (var i in data.products) {
