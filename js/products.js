dcbfc4b4f9991e11a8662d3951acc2012770cb8f
Tuxuy3Dom <ihor.yaremchuk2120@gmail.com>
2023-07-02 15:00:10 +0200

    Add new img, products.js file, navbar.html file and header.html

---
+++ js/products.js
@@ -0,0 +1,38 @@
+const products = [
+{
+    id: 0,
+    name: "Drut spawalniczy Tysweld T20.003 5 kg",
+    description: "Drut elektrodowy manganowo – krzemowy, miedziowany, przeznaczony do spawania metodą MIG/MAG stali niskowęglowych konstrukcyjnych oraz drobnoziarnistych stali węglowo manganowych.",
+    category: "drut",
+    price: 60.00,
+    image: "./img/drut0-8.jpg",
+    sale: true,
+    saleAmount: 56, 
+},
+{
+    id: 1,
+    name: "Zacisk Rhinoweld GC17-00-00-600 600 A",
+    description: "Solidny zacisk spawalniczy wykonany z grubego odlewu mosiężnego zapewnia dobre przewodzenie prądu. Odporny na uszkodzenia i uszczerbki mechaniczne. Dzięki zastosowaniu mosiądzu szerokość uchwytu jest zredukowana do minimum zachowując bardzo dobre przewodzenie prądu.",
+    category: "zacisk",
+    price: 50.00,
+    image: "./img/zacisk17-600.jpg", 
+},
+{
+    id: 2,
+    name: "Zacisk Rhinoweld GC08-00-00-200 200 A",
+    description: "Zacisk kleszczowy zapewnia bardzo dobre przewodzenie prądu, a jego wykonanie z metalu sprawia, że jest to wyrób solidny i odporny na uszkodzenia i uszczerbki mechaniczne. Przeznaczony do mocowania przewodu spawalniczego jako jednego z biegunów źródła prądu z materiałem spawanym. Zakończenie zacisku wykonane zostało z miedzianych końcówek. Mocowany jest do jednej ze szczęk przy pomocy połączenia śrubowego.",
+    category: "zacisk",
+    price: 18.00,
+    image: "./img/zacisk17-600.jpg",
+},
+{
+    id: 3,
+    name: "Zacisk Rhinoweld GC09-00-00-300 300 A",
+    description: "Zacisk kleszczowy zapewnia bardzo dobre przewodzenie prądu, a jego wykonanie z metalu sprawia, że jest to wyrób solidny i odporny na uszkodzenia i uszczerbki mechaniczne.",
+    category: "zacisk",
+    price: 24.00,
+    image: "./img/zacisk17-600.jpg",
+    sale: true,
+    saleAmount: 22, 
+},
+]
